318d14df909d30e61baf8562bf91bd83a5e39808
codegun2019 <codegun.com@gmail.com>
2024-02-10 09:44:34 +0700

    Vue3 Ref Side Effect and method

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,33 @@
 <template>
   <div><h1>CODEGUN</h1></div>
-  <h2>#Debug:{{ count }}</h2>
-  <h2>#Ref:{{ refcount }}</h2>
+  <span>#Debug:{{ count }}</span>
+  <span>#Ref:{{ refcount }}</span>
+  <button @click="onClickAdd">Add</button>
+  <button @click="onClickAddref">AddRef</button>
 </template>
 <script lang="ts">
 import { defineComponent, ref } from "vue";
 
 export default defineComponent({
   setup() {
-    let count = 20; //เปลี่ยนแปลงค่าได้
-    const refcount   = ref<number>(2)
+    let count = 20; //เปลี่ยนแปลงค่าได้ no side effect vue ไม่ส่งใน template
+    const refcount   = ref<number>(2) //side effect
+
+    const onClickAdd = ()=> {
+        count = count + 1
+        console.log("count = " + count)
+    }
+
+    const onClickAddref = ()=> {
+        refcount.value = refcount.value + 1 //.value เสมอ
+        console.log("count = " + refcount.value)
+    }
 
     return {
       count,
-      refcount
+      refcount,
+      onClickAdd,
+      onClickAddref
 
     };
   },
